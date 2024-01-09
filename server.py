7a99acda6018bcedcbdac0f045b1a1d803bbc7d1
x-student-x <148026385+x-student-x@users.noreply.github.com>
2024-01-09 14:28:24 -0600

    server setup

---
+++ server.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, request
+from EmotionDetection.emotion_detection import emotion_detector
+
+app = Flask("Emotion Detector")
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = sentiment_analyzer(text_to_analyze)
+    label = response['label']
+    score = response['score']
+    return "The given text has been identified as {} with a score of {}.".format(label.split('_')[1], score)
+
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
